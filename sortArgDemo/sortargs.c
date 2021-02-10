14da167089b492affc389ae19b111f553243fd2b
student <vherrera@miners.utep.edu>
2021-02-09 23:20:03 -0700

    First Commit

--- sortArgDemo/sortargs.c
+++ sortArgDemo/sortargs.c
@@ -2,6 +2,8 @@
   
 int main(int argc, char **argv)	/* prints arguments in order */
 {
+ $ my fancy interface thing
+   
   for (char **p = &argv[1]; *p; p++)
     bstInsert(&bstRoot, *p);
   bstPrint(bstRoot);
---
+++ src/Uimain.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <stlib.h>
+# include "tokenizer.h"
+# include "history.h"
+
+
+main(){
+  char word[200];
+  printf("Enter what you desire");
+  gets(word);
+  puts(name);
+  return 0;
+  
+  
+}
+
