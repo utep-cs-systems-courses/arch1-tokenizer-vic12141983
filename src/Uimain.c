07591f4629ca233b067948aa5b4cbba82a6ff5e4
student <vherrera@miners.utep.edu>
2021-02-18 09:15:18 -0700

    Count words complete

--- src/Uimain.c
+++ src/Uimain.c
@@ -1,16 +1,22 @@
 # include <stdio.h>
-# include <stlib.h>
+# include <stdlib.h>
 # include "tokenizer.h"
 # include "history.h"
 
 
-main(){
-  char word[200];
-  printf("Enter what you desire");
-  gets(word);
-  puts(name);
-  return 0;
+int main(){
+  char word[100];
   
   
-}
+  
+  printf("$");
+  fgets(word,100,stdin);
+  puts(word);
+  
+  
 
+   
+   printf("Now checking for empty spaces %d \n",space_char(word[1]));
+   printf("now checking for non empty spaces %d \n" ,non_space_char(word[1]));
+   printf("Now counting number of  words in string %d \n",count_words(word));
+}
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,28 +1,54 @@
-#ifndef _TOKENIZER_
-#define _TOKENIZER_
+#include <stdio.h>
+#include  <stdlib.h>
+#include "tokenizer.h"
 
+#define in 1 /* inside a word*/
+#define out 0 /* outside a word */
 
-/* Return true (non-zero) if c is a whitespace characer
-   ('\t' or ' ').  
-   Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c){
+ 
+  return (c==' ' || c=='\t'&& c!= '\0' );
+  
+}
  
 /* 
 Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-  int non_space_char(char c);
+int non_space_char(char c){
+  return (c!=' '|| c!='\t'&& c!='\0');
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-  char word_start(char *str); 
+char word_start(char *str){
+  if( *str == ' ');
+  return 0;
+  
+  
+} 
 
 /* Returns a pointer terminator char following *word */
-  char *word_terminator(char *word);
+char *word_terminator(char *word);
 
 /* Counts the number of words in the string argument. */
-int count_words(char *str);
+int count_words(char *str){
+  int state =out;
+  int wor_count =0;
+  while(*str){
+    if(*str == ' ' || *str =='\n' || *str =='\t')
+      state =out;
+    else if(state == out){
+      state = in;
+      ++wor_count;
+    }
+    ++str;
+
+  }
+  return wor_count;
+
+}
 
 /* Returns a fresly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
@@ -44,5 +70,3 @@ void print_tokens(char **tokens);
 
 /* Frees all tokens and the vector containing themx. */
 void free_tokens(char **tokens);
-
-#endif
