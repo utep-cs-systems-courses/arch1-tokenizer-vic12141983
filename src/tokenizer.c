a1c489fda0262ce7729353789f1aa2332a3fc03e
student <vherrera@miners.utep.edu>
2021-02-25 01:35:37 -0700

    update word start and terminator

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,12 +2,11 @@
 #include  <stdlib.h>
 #include "tokenizer.h"
 
-#define in 1 /* inside a word*/
-#define out 0 /* outside a word */
 
+/*return true for space */
 int space_char(char c){
  
-  return (c==' ' || c=='\t'&& c!= '\0' );
+  return (c ==' ' || c =='\t'&& c!= '\0' );
   
 }
  
@@ -16,37 +15,42 @@ Return true (non-zero) if c is a non-whitespace
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
 int non_space_char(char c){
-  return (c!=' '|| c!='\t'&& c!='\0');
+  return (c!=' '&& c!='\t'&& c!='\0');
 }
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char word_start(char *str){
-  if( *str == ' ');
+char *word_start(char *str){
+  while(*str){
+    if(non_space_char(*str)){
+      return str;
+    }
+    str++; 
+  }
   return 0;
-  
-  
-} 
 
-/* Returns a pointer terminator char following *word */
-char *word_terminator(char *word);
+}
+
+/* Returns a pointer terminator char following *word
+ Return a zero pointer if str does not contain any words.  */
+char *word_terminator(char *word){
+  while(*word){
+    if(space_char(*word){
+	return word;
+      }
+      word++;
+  }
+  return 0;        
+}
 
 /* Counts the number of words in the string argument. */
 int count_words(char *str){
-  int state =out;
-  int wor_count =0;
-  while(*str){
-    if(*str == ' ' || *str =='\n' || *str =='\t')
-      state =out;
-    else if(state == out){
-      state = in;
-      ++wor_count;
-    }
-    ++str;
 
-  }
-  return wor_count;
+  int count =0;
+  while(*str){
+}
+  hello    world       
 
 }
 
