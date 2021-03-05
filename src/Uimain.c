6616cf8bc5e32616c3d6830a54dfbddb1597d3cc
student <vherrera@miners.utep.edu>
2021-03-04 21:53:50 -0700

    Main added

--- src/Uimain.c
+++ src/Uimain.c
@@ -7,7 +7,7 @@ int main(){
   char word[100];
     
   
-  printf("Please enter a word: ");
+  printf("Please enter a word:");
   fgets(word,100,stdin);
   puts(word);
  
@@ -19,10 +19,12 @@ int main(){
    printf("Spaces afer the each word: %d \n" ,non_space_char(word[1]));
    printf("Returning word after space : % s \n", word_start(word));
    printf("Now counting number of  words in string: %d \n",count_words(word));
-   printf("Now copying in String in freshly new String: % s \n ",copy_str(word,19));
+   printf("Now copying in String in freshly new String: % s \n",copy_str(word,19));
    
    char** test = tokenize(word);
    print_tokens(test);
+   free_tokens(test);
+   
    
 
 }
